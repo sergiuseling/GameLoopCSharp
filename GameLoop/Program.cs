440c5bbe43e331fc3eb08eea3d22188bd18937c3
sergiu.seling <sergiu.seling@ro.neusoft.com>
2013-08-01 17:10:41 +0300

    Initial

---
+++ GameLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameLoop
+{
+    static class Program
+    {
+        static FastLoop _fastLoop = new FastLoop(GameLoop);
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static void GameLoop(double elapsedTime)
+        {
+            //GetInput
+            //Process
+            //Render
+            System.Console.WriteLine("Game Loop");
+        }
+    }
+}
